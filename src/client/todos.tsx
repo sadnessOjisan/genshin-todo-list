46bc78a286a1aa1d12a953c3be371664913277df
sadnessOjisan <sadness.ojisan@gmail.com>
2023-04-16 03:58:33 +0900

    chore: configure daily mission

--- src/client/todos.tsx
+++ src/client/todos.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { ChangeEvent, FC, useEffect, useState } from "react";
+import { ChangeEvent, FC, useEffect, useMemo, useState } from "react";
+import { TODO_DATA, TODO_KEY } from "../data/todo-data";
 import type { Lang } from "../type/lang";
+import { strictEntries } from "../util/object";
 
 interface Props {
   lang: Lang;
@@ -9,19 +11,24 @@ interface Props {
 
 export const Todos: FC<Props> = ({ lang }) => {
   const [state, setState] = useState<{
-    mission1: string | null;
-    mission2: string | null;
+    [x in TODO_KEY]: string | null;
   }>({
     mission1: null,
     mission2: null,
+    mission3: null,
+    mission4: null,
   });
   useEffect(() => {
     const date1 = window.localStorage.getItem("mission1");
     const date2 = window.localStorage.getItem("mission2");
+    const date3 = window.localStorage.getItem("mission3");
+    const date4 = window.localStorage.getItem("mission4");
 
     setState({
       mission1: date1,
       mission2: date2,
+      mission3: date3,
+      mission4: date4,
     });
   }, []);
 
@@ -38,30 +45,36 @@ export const Todos: FC<Props> = ({ lang }) => {
       setState({ ...state, [value]: null });
     }
   };
+
+  const daily = useMemo(() => {
+    return strictEntries(TODO_DATA)
+      .filter((v) => v[1].category === "daily_mission")
+      .map((v) => ({
+        key: v[0],
+        value: v[1].name(lang),
+        logic: v[1].logic.descriptipon(lang),
+      }));
+  }, [lang]);
   return (
     <div>
       {lang}
       <h2>daily</h2>
       {JSON.stringify(state)}
       <ul>
-        <li>
-          <input
-            type="checkbox"
-            id="mission1"
-            onChange={handleChangeCheckbox}
-            value="mission1"
-          />
-          <label htmlFor="mission1">mission 1({state.mission1})</label>
-        </li>
-        <li>
-          <input
-            type="checkbox"
-            id="mission2"
-            onChange={handleChangeCheckbox}
-            value="mission2"
-          />
-          <label htmlFor="mission2">mission 2({state.mission2})</label>
-        </li>
+        {daily.map((d) => (
+          <li>
+            <input
+              type="checkbox"
+              id={d.key}
+              onChange={handleChangeCheckbox}
+              value={d.key}
+            />
+            <label htmlFor={d.key}>
+              {d.value}({state[d.key]})
+            </label>
+            <span>{d.logic}</span>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ src/data/todo-data.ts
@@ -0,0 +1,156 @@
+import { Lang } from "../type/lang";
+
+type Cateogory = "daily_mission";
+export type TODO_KEY = "mission1" | "mission2" | "mission3" | "mission4";
+
+interface Logic {
+  descriptipon: (lang: Lang) => string;
+  /**
+   * TODO がリセットされたか判断する関数。
+   * ユーザーがTODOを完了した日付を受け取り、そのTODOがリセットされたかどうかを判断する。
+   * @params lastUpdated ユーザーがTODOを完了した日付
+   * @return boolean TODOをリセットすべきなら true そうでないなら false
+   */
+  func: (lastUpdated: Date) => boolean;
+}
+
+interface TODO_VALUE {
+  description: string;
+
+  logic: Logic;
+  name: (lang: Lang) => string;
+  category: Cateogory;
+}
+
+/**
+ * デイリーミッションのTODOチェック解除ロジック
+ * * TODO完了時点の次の AM5:00 に解除される。
+ * @param lastUpdated lastUpdated ユーザーがTODOを完了した日付
+ * @returns boolean TODOをリセットすべきなら true そうでないなら false
+ */
+const dailyLogic = (lastUpdated: Date) => {
+  return false;
+};
+
+export const TODO_DATA: Record<TODO_KEY, TODO_VALUE> = {
+  /**
+   * １つ目のデイリーミッション
+   */
+  mission1: {
+    description: "daily mission(1)",
+    logic: {
+      descriptipon: (lang) => {
+        switch (lang) {
+          case "ja":
+            return "TODO完了時点の次の AM5:00 に解除される。";
+          case "en":
+            return "next am 5:00";
+          default:
+            return "TODO完了時点の次の AM5:00 に解除される。";
+        }
+      },
+      func: dailyLogic,
+    },
+    category: "daily_mission",
+    name: (lang) => {
+      switch (lang) {
+        case "ja":
+          return "デイリーミッション1";
+        case "en":
+          return "daily mission1";
+        default:
+          return "デイリーミッション1";
+      }
+    },
+  },
+  /**
+   * ２つ目のデイリーミッション
+   */
+  mission2: {
+    description: "daily mission(2)",
+    logic: {
+      descriptipon: (lang) => {
+        switch (lang) {
+          case "ja":
+            return "TODO完了時点の次の AM5:00 に解除される。";
+          case "en":
+            return "next am 5:00";
+          default:
+            return "TODO完了時点の次の AM5:00 に解除される。";
+        }
+      },
+      func: dailyLogic,
+    },
+    category: "daily_mission",
+    name: (lang) => {
+      switch (lang) {
+        case "ja":
+          return "デイリーミッション2";
+        case "en":
+          return "daily mission2";
+        default:
+          return "デイリーミッション2";
+      }
+    },
+  },
+  /**
+   * 3つ目のデイリーミッション
+   */
+  mission3: {
+    description: "daily mission(2)",
+    logic: {
+      descriptipon: (lang) => {
+        switch (lang) {
+          case "ja":
+            return "TODO完了時点の次の AM5:00 に解除される。";
+          case "en":
+            return "next am 5:00";
+          default:
+            return "TODO完了時点の次の AM5:00 に解除される。";
+        }
+      },
+      func: dailyLogic,
+    },
+    category: "daily_mission",
+    name: (lang) => {
+      switch (lang) {
+        case "ja":
+          return "デイリーミッション3";
+        case "en":
+          return "daily mission3";
+        default:
+          return "デイリーミッション3";
+      }
+    },
+  },
+  /**
+   * 4つ目のデイリーミッション
+   */
+  mission4: {
+    description: "daily mission(2)",
+    logic: {
+      descriptipon: (lang) => {
+        switch (lang) {
+          case "ja":
+            return "TODO完了時点の次の AM5:00 に解除される。";
+          case "en":
+            return "next am 5:00";
+          default:
+            return "TODO完了時点の次の AM5:00 に解除される。";
+        }
+      },
+      func: dailyLogic,
+    },
+    category: "daily_mission",
+    name: (lang) => {
+      switch (lang) {
+        case "ja":
+          return "デイリーミッション4";
+        case "en":
+          return "daily mission4";
+        default:
+          return "デイリーミッション4";
+      }
+    },
+  },
+};
