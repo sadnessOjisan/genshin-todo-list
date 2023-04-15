3e2349b497113024e94268a79f8b8c9e8e987064
sadnessOjisan <sadness.ojisan@gmail.com>
2023-04-16 02:26:08 +0900

    chore: generate each page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,12 +1,24 @@
 import { FC } from "react";
 
-const Page: FC = () => {
+interface RoutePath {
+  lang: "ja" | "en";
+}
+
+type Props = {
+  params: RoutePath;
+};
+
+const Page: FC<Props> = (props) => {
   return (
     <div>
       <h1>hello world</h1>
-      <div>yeah</div>
+      <div>lang: {JSON.stringify(props.params.lang)}</div>
     </div>
   );
 };
+
+export async function generateStaticParams(): Promise<RoutePath[]> {
+  return (["ja", "en"] as const).map((lang) => ({ lang }));
+}
 
 export default Page;
