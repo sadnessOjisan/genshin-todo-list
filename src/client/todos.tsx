4fd9d160a85f59cdff324a934fee193a59501290
sadnessOjisan <sadness.ojisan@gmail.com>
2023-04-19 22:38:18 +0900

    chore: configure weekly boss

--- src/client/todos.tsx
+++ src/client/todos.tsx
@@ -17,18 +17,27 @@ export const Todos: FC<Props> = ({ lang }) => {
     mission2: null,
     mission3: null,
     mission4: null,
+    weekly_boss1: null,
+    weekly_boss2: null,
+    weekly_boss3: null,
   });
   useEffect(() => {
     const date1 = window.localStorage.getItem("mission1");
     const date2 = window.localStorage.getItem("mission2");
     const date3 = window.localStorage.getItem("mission3");
     const date4 = window.localStorage.getItem("mission4");
+    const weeklyBoss1ExpiredDate = window.localStorage.getItem("weekly_boss1");
+    const weeklyBoss2ExpiredDate = window.localStorage.getItem("weekly_boss2");
+    const weeklyBoss3ExpiredDate = window.localStorage.getItem("weekly_boss3");
 
     setState({
       mission1: date1,
       mission2: date2,
       mission3: date3,
       mission4: date4,
+      weekly_boss1: weeklyBoss1ExpiredDate,
+      weekly_boss2: weeklyBoss2ExpiredDate,
+      weekly_boss3: weeklyBoss3ExpiredDate,
     });
   }, []);
 
@@ -55,6 +64,16 @@ export const Todos: FC<Props> = ({ lang }) => {
         logic: v[1].logic.descriptipon(lang),
       }));
   }, [lang]);
+
+  const weekly = useMemo(() => {
+    return strictEntries(TODO_DATA)
+      .filter((v) => v[1].category === "weekly_mission")
+      .map((v) => ({
+        key: v[0],
+        value: v[1].name(lang),
+        logic: v[1].logic.descriptipon(lang),
+      }));
+  }, [lang]);
   return (
     <div>
       {lang}
@@ -62,6 +81,23 @@ export const Todos: FC<Props> = ({ lang }) => {
       {JSON.stringify(state)}
       <ul>
         {daily.map((d) => (
+          <li>
+            <input
+              type="checkbox"
+              id={d.key}
+              onChange={handleChangeCheckbox}
+              value={d.key}
+            />
+            <label htmlFor={d.key}>
+              {d.value}({state[d.key]})
+            </label>
+            <span>{d.logic}</span>
+          </li>
+        ))}
+      </ul>
+      <h2>weekly</h2>
+      <ul>
+        {weekly.map((d) => (
           <li>
             <input
               type="checkbox"
--- src/data/todo-data.ts
+++ src/data/todo-data.ts
@@ -1,7 +1,15 @@
 import { type Lang } from "../type/lang";
 
-type Cateogory = "daily_mission";
-export type TODO_KEY = "mission1" | "mission2" | "mission3" | "mission4";
+type Cateogory = "daily_mission" | "weekly_mission";
+
+export type TODO_KEY =
+  | "mission1"
+  | "mission2"
+  | "mission3"
+  | "mission4"
+  | "weekly_boss1"
+  | "weekly_boss2"
+  | "weekly_boss3";
 
 interface Logic {
   descriptipon: (lang: Lang) => string;
@@ -29,6 +37,17 @@ interface TODO_VALUE {
  * @returns boolean TODOをリセットすべきなら true そうでないなら false
  */
 const dailyLogic = (lastUpdated: Date) => {
+  console.log(lastUpdated);
+  return false;
+};
+
+/**
+ * weeklyのTODOチェック解除ロジック
+ * * TODO完了時点の次の AM5:00 に解除される。
+ * @param lastUpdated lastUpdated ユーザーがTODOを完了した日付
+ * @returns boolean TODOをリセットすべきなら true そうでないなら false
+ */
+const weeklyLogic = (lastUpdated: Date) => {
   console.log(lastUpdated);
   return false;
 };
@@ -128,7 +147,7 @@ export const TODO_DATA: Record<TODO_KEY, TODO_VALUE> = {
    * 4つ目のデイリーミッション
    */
   mission4: {
-    description: "daily mission(2)",
+    description: "daily mission(4)",
     logic: {
       descriptipon: (lang) => {
         switch (lang) {
@@ -151,6 +170,87 @@ export const TODO_DATA: Record<TODO_KEY, TODO_VALUE> = {
           return "daily mission4";
         default:
           return "デイリーミッション4";
+      }
+    },
+  },
+  weekly_boss1: {
+    description: "weekly boss (1)",
+    logic: {
+      descriptipon: (lang) => {
+        switch (lang) {
+          case "ja":
+            return "毎週月曜日 AM5:00 に解除される。";
+          case "en":
+            return "reset at AM 5:00 on every monday";
+          default:
+            return "毎週月曜日 AM5:00 に解除される。";
+        }
+      },
+      func: weeklyLogic,
+    },
+    category: "weekly_mission",
+    name: (lang) => {
+      switch (lang) {
+        case "ja":
+          return "週次ボス(1)";
+        case "en":
+          return "weekly boss(1)";
+        default:
+          return "週次ボス(1)";
+      }
+    },
+  },
+  weekly_boss2: {
+    description: "weekly boss (2)",
+    logic: {
+      descriptipon: (lang) => {
+        switch (lang) {
+          case "ja":
+            return "毎週月曜日 AM5:00 に解除される。";
+          case "en":
+            return "reset at AM 5:00 on every monday";
+          default:
+            return "毎週月曜日 AM5:00 に解除される。";
+        }
+      },
+      func: weeklyLogic,
+    },
+    category: "weekly_mission",
+    name: (lang) => {
+      switch (lang) {
+        case "ja":
+          return "週次ボス(2)";
+        case "en":
+          return "weekly boss(2)";
+        default:
+          return "週次ボス(2)";
+      }
+    },
+  },
+  weekly_boss3: {
+    description: "weekly boss (3)",
+    logic: {
+      descriptipon: (lang) => {
+        switch (lang) {
+          case "ja":
+            return "毎週月曜日 AM5:00 に解除される。";
+          case "en":
+            return "reset at AM 5:00 on every monday";
+          default:
+            return "毎週月曜日 AM5:00 に解除される。";
+        }
+      },
+      func: weeklyLogic,
+    },
+    category: "weekly_mission",
+    name: (lang) => {
+      switch (lang) {
+        case "ja":
+          return "週次ボス(3)";
+        case "en":
+          return "weekly boss(3)";
+        default:
+          return "週次ボス(3)";
       }
     },
   },
